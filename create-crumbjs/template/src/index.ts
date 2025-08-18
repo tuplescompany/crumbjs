46900919fe2dcc9a7a0ffc2eca4bf5ad141e685d
Elias Noya <elias@tupleshr.com>
2025-08-18 11:04:18 -0300

    update create-crumbjs script

--- create-crumbjs/template/src/index.ts
+++ create-crumbjs/template/src/index.ts
@@ -1,7 +1,8 @@
 import { App, cors, secureHeaders, signals } from '@crumbjs/core';
 import { indexController } from './modules/index/index.controller';
 
-const app = new App({ prefix: 'api' })
+export default new App()
+	.prefix('api')
 	.use(cors({ origin: '*' }))
 	.use(signals(true))
 	.use(secureHeaders())
