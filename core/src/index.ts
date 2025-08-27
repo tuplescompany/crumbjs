f54cb1b4c235f5f3941a1bd2b367784218220204
Elias Noya <elias@tupleshr.com>
2025-08-27 16:38:22 -0300

    add codecs helpers

--- core/src/index.ts
+++ core/src/index.ts
@@ -20,3 +20,4 @@ export { cors } from './middlewares/cors';
 export { signals } from './middlewares/signals';
 export { secureHeaders } from './middlewares/secure-headers';
 export { validate, safeValidate, validateAsync, safeValidateAsync } from './validator';
+export { codecs } from './helpers/codecs';
